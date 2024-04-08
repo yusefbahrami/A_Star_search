b694a9a772f6e62a429393094da215af3ed99116
yusefbahrami <yusef.b1381@gmail.com>
2024-04-08 05:41:16 +0330

    Improved function return data

--- goal_test.py
+++ goal_test.py
@@ -3,8 +3,14 @@ from graph import Node
 
 # the Goal_Test function should be defined statically and be different for each problem
 # and receive different data and return the desired data.
+
+goal_node = None
+
+
 def goal_test(currentNode: Node, goalNode: Node = None):
-    goal_node = goalNode
+    if goal_node == None:
+        goal_node = goalNode
+
     if currentNode == goal_node:
-        return True
-    return False
+        return [goal_node, True]
+    return [goal_node, False]
